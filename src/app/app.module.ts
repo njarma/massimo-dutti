c027c4ae1ad1d2deaa51bb11f9b70fe6e90535d6
Nicolás Jarma <njarma@evoltis.com>
2020-07-09 07:38:56 -0300

    creación de una primera versión del router de angular

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,24 +1,15 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
 import { AppComponent } from './app.component';
-import { LoginComponent } from './login/login.component';
-import { RegisterComponent } from './register/register.component';
-import { ShipsComponent } from './ships/ships.component';
-import { StarshipsListComponent } from './ships/starships-list/starships-list.component';
-import { StarshipComponent } from './ships/starships-list/starship/starship.component';
+import { AppRoutingModule } from './components/routes.module';
 
 @NgModule({
   declarations: [
-    AppComponent,
-    LoginComponent,
-    RegisterComponent,
-    ShipsComponent,
-    StarshipsListComponent,
-    StarshipComponent
+    AppComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    AppRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/components/routes.module.ts
@@ -0,0 +1,25 @@
+import { NgModule } from '@angular/core';
+import { RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+
+export const routes = [
+    { path: '', redirectTo: '/login', pathMatch: 'full' },
+    { path: 'login', component: LoginComponent },
+    { path: 'register', component: RegisterComponent },
+
+    // Not found
+    { path: '**', redirectTo: 'login' }
+
+];
+
+@NgModule({
+    imports: [
+        RouterModule.forRoot(routes, { useHash: true })
+    ],
+    exports: [
+        RouterModule
+    ]
+})
+
+export class AppRoutingModule {}

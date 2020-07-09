5cf85995c3c9a2aeed85f2e909e5146ecaf8a199
Nicolás Jarma <njarma@evoltis.com>
2020-07-09 09:02:52 -0300

    configuración del router del proyecto

--- src/app/components/routes.module.ts
+++ src/app/components/routes.module.ts
@@ -1,12 +1,18 @@
-import { NgModule } from '@angular/core';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
+import { ShipsComponent } from './ships/ships.component';
+import { StarshipComponent } from './ships/starships-list/starship/starship.component';
+import { StarshipsListComponent } from './ships/starships-list/starships-list.component';
 
 export const routes = [
     { path: '', redirectTo: '/login', pathMatch: 'full' },
     { path: 'login', component: LoginComponent },
     { path: 'register', component: RegisterComponent },
+    { path: 'ships', component: ShipsComponent },
+    { path: 'starships', component: StarshipsListComponent },
+    { path: 'starship', component: StarshipComponent },
 
     // Not found
     { path: '**', redirectTo: 'login' }

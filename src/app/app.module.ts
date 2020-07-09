8dd3cbf29e86bff7d60b625d2c8f3216a1495b74
Nicolás Jarma <njarma@evoltis.com>
2020-07-09 05:42:57 -0300

    Creación del esqueleto del proyecto, y modificación del favico oficial de Massimo Dutti

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,20 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { RegisterComponent } from './register/register.component';
+import { ShipsComponent } from './ships/ships.component';
+import { StarshipsListComponent } from './ships/starships-list/starships-list.component';
+import { StarshipComponent } from './ships/starships-list/starship/starship.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    RegisterComponent,
+    ShipsComponent,
+    StarshipsListComponent,
+    StarshipComponent
   ],
   imports: [
     BrowserModule
---
+++ src/app/ships/starships-list/starships-list.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-starships-list',
+  templateUrl: './starships-list.component.html',
+  styleUrls: ['./starships-list.component.sass']
+})
+export class StarshipsListComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}

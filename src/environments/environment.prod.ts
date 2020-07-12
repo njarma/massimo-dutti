b4c87cc52f5b4965918766b5391cdbc0bd77a092
Nicolás Jarma <njarma@evoltis.com>
2020-07-12 09:25:02 -0300

    se creó la carpeta docs, para realizar el despliegue en github

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
 export const environment = {
-  production: true
+  production: true,
+  ambientes: {
+    desarrolloMock: {
+      apiUrlBase: 'app/mocks/',
+      esMock: true
+    },
+    apiDevelop: {
+        apiUrlBase: 'https://swapi.dev/api/',
+        imageUrlBase: 'https://starwars-visualguide.com/assets/img/starships/'
+    },
+    seleccionado: 'apiDevelop'
+  }
 };
